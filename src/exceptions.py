721cb2a99b241e90e0fa5c56570a8c57c12b2d03
Avinash <avinashmbhat18@gmail.com>
2023-05-14 12:02:09 +0530

    Improved Error Handeling

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,9 +1,9 @@
 import sys
-
+from src.logger import logging
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
-    error_message=f"Error is there in python script namely{file_name} in line number{exc_tb.tb_lineno} error message {str(error)}"
+    error_message=f"\nError_File->{file_name} \nERROR_Line->{exc_tb.tb_lineno} \nERROR->{str(error)}"
     return error_message
 
 class custom_exception(Exception):
@@ -11,5 +11,6 @@ class custom_exception(Exception):
         self.error_message=error_message_detail(error_message,error_detail)
     def __str__(self) -> str:
         return self.error_message
+    
 
     
